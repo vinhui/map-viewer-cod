ae98ca41ae0d302333cc992f35f1f9d0e3bc6db4
vinhui <vinni.vinni.vin@gmail.com>
2024-10-26 10:54:53 +0200

    The fake filesystem can now return all files in a directory, also now as a case insensitive option

--- src/LibBSP/FakeFileSystem.ts
+++ src/LibBSP/FakeFileSystem.ts
@@ -1,15 +1,52 @@
 export class FakeFileSystem {
+    /**
+     * Set this before calling the Download functions
+     */
+    public static caseInsensitive = true
     private static fileData = new Map<string, Uint8Array>()
 
     public static FileExists(path: string): boolean {
+        if (this.caseInsensitive) {
+            path = path.toLowerCase()
+        }
         return this.fileData.has(path)
     }
 
     public static ReadFile(path: string): Uint8Array {
+        if (this.caseInsensitive) {
+            path = path.toLowerCase()
+        }
         return this.fileData.get(path)
     }
 
+    public static GetFilesInDirectory(path: string, regex?: RegExp): string[] {
+        if (path.startsWith('/')) {
+            path = path.substring(1)
+        }
+
+        if (this.caseInsensitive) {
+            path = path.toLowerCase()
+        }
+
+        const items: string[] = []
+        for (let filePath of this.fileData.keys()) {
+            if (filePath.startsWith(path)) {
+                if (!regex) {
+                    items.push(filePath)
+                } else if (regex.test(filePath)) {
+                    items.push(filePath)
+                }
+            }
+        }
+
+        return items
+    }
+
     public static Unload(path: string): void {
+        if (this.caseInsensitive) {
+            path = path.toLowerCase()
+        }
+
         if (this.fileData.has(path)) {
             this.fileData.delete(path)
         }
@@ -20,9 +57,18 @@ export class FakeFileSystem {
     }
 
     public static async DownloadFile(baseUrl: string, path: string): Promise<boolean> {
+        if (path.startsWith('/')) {
+            path = path.substring(1)
+        }
+
         if (this.FileExists(path)) {
             return true
         }
+
+        if (!baseUrl.endsWith('/')) {
+            baseUrl += '/'
+        }
+
         const response = await fetch(baseUrl + path)
         if (!response.ok) {
             return false
