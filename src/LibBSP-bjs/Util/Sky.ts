dcfbe70b917d7d9812b6cb3167f1d0ff70d4711f
vinhui <vinni.vinni.vin@gmail.com>
2024-11-10 20:11:10 +0100

    Removed unused function and package

--- src/LibBSP-bjs/Util/Sky.ts
+++ src/LibBSP-bjs/Util/Sky.ts
@@ -1,65 +1,6 @@
-import {CubeTexture, MeshBuilder, Scene, StandardMaterial, Texture, TransformNode} from '@babylonjs/core'
-import {FakeFileSystem, File} from 'libbsp-js'
-import mime from 'mime'
+import {MeshBuilder, Scene, StandardMaterial, Texture, TransformNode} from '@babylonjs/core'
+import {FakeFileSystem} from 'libbsp-js'
 import {loadTextureAtPath} from './texture'
-
-function loadFilesIntoCubeTexture(path: string, cubeTex: CubeTexture, onTexChanged: () => void) {
-    type Sides = 'bk' | 'dn' | 'ft' | 'lf' | 'rt' | 'up'
-
-    const files = new Map<Sides, File>()
-    const sides: Sides[] = ['bk', 'dn', 'ft', 'lf', 'rt', 'up']
-    for (let side of sides) {
-        let f: File[]
-        f = FakeFileSystem.FindFiles(`${path}_${side}.`, null, false)
-        if (f?.length === 0) {
-            console.warn(`Couldn't find skybox texture`, path)
-            return null
-        }
-        files.set(side, f[0])
-    }
-
-    Promise.all(files.values().map(x => x.download()))
-        .then(x => {
-            if (x.some(x => !x)) {
-                return
-            }
-
-            const blobUrls = new Map<Sides, string>()
-            for (let [side, file] of files.entries()) {
-                const blob = new Blob([file.bytes], {type: mime.getType(file.nameWithExtension)})
-                blobUrls.set(side, URL.createObjectURL(blob))
-            }
-            const cleanup = (e?: unknown) => {
-                for (let url of blobUrls.values()) {
-                    URL.revokeObjectURL(url)
-                }
-            }
-            cubeTex.updateURL(
-                '', // url
-                null, // forced extension
-                () => {
-                    cleanup()
-                    onTexChanged()
-                }, // onload
-                null, // prefiltered
-                (e) => {
-                    console.error('Failed to load sky texture:', e)
-                    cleanup()
-                }, // onerror
-                null, // extensions
-                null, // delayload
-                [
-                    blobUrls.get('rt'),
-                    blobUrls.get('up'),
-                    blobUrls.get('ft'),
-                    blobUrls.get('lf'),
-                    blobUrls.get('dn'),
-                    blobUrls.get('bk'),
-                ],
-                null, // buffer
-            )
-        })
-}
 
 function getEnvTextureFromShaderName(texturePath: string, callback: (file: string) => void) {
     const scriptFiles = FakeFileSystem.FindFiles('scripts/', /\.shader$/i, false)
@@ -162,6 +103,5 @@ export function buildSkybox(texturePath: string, scene: Scene) {
             }
             sidesMap.get(side).emissiveTexture = tex
         }
-
     })
 }
